14c49d2a25747c4e4720fcdf3caf9f451b6786e4
saeed <you@example.com>
2018-04-26 11:50:23 +0430

    make some migration & include urls for customer and store

--- hinata/urls.py
+++ hinata/urls.py
@@ -31,6 +31,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(api_urlpatterns)),
     path('', include('frontview.urls')),
+    path('store/', include('store.urls')),
+    path('customer/', include('customer.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
