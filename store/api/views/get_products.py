7609105a7b2d8db70e85e4bfbb953475e09f8c7f
saeed <you@example.com>
2018-04-26 12:07:17 +0430

    fixed get category

--- store/api/views/get_products.py
+++ store/api/views/get_products.py
@@ -1,10 +1,11 @@
 from json import loads as json_loads
 
+from django.shortcuts import get_object_or_404
 from django.http.response import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.core.paginator import Paginator
 
-from store.models import Product
+from store.models import Product, Category
 from ratelimit.decorators import ratelimit
 
 
@@ -15,8 +16,8 @@ def get_products(request):
     this_page_number = int(request.GET.get('page', '1'))
     count = int(request.GET.get('count', '12'))
 
-    category_name = request.GET.get('category')
-    all_products = Product.objects.filter(category__name=category_name)
+    category = get_object_or_404(Category, name=request.GET.get('category'))
+    all_products = Product.objects.filter(category=category)
     all_pages = Paginator(all_products, count)
     requested_page = all_pages.page(this_page_number)
     context = {
