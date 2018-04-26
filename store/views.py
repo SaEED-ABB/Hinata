379097ad4c0e86e6e1f79436cb3058945d19042b
saeed <you@example.com>
2018-04-26 10:24:33 +0430

    refactoring urls, views, templates

--- store/views.py
+++ store/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def products(request):
     context = {'title': 'محصولات'}
-    return render(request, 'frontview/products.html', context)
+    return render(request, 'store/products.html', context)
