d7b14e8a6dcf169155614bf5a1b6f1162ad38d0f
saeed <you@example.com>
2018-05-23 14:24:21 +0430

    limit size on product image uploads & delete associated image files while deleting product

--- store/admin.py
+++ store/admin.py
@@ -15,11 +15,52 @@ class PropertyInline(admin.TabularInline):
 class ProductAdmin(admin.ModelAdmin):
     inlines = [ProductImageInline, PropertyInline]
 
+    actions = ['really_delete_selected']
+
+    def get_actions(self, request):
+        actions = super(ProductAdmin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
+
+    def really_delete_selected(self, request, queryset):
+        for obj in queryset:
+            obj.delete()
+
+        if queryset.count() == 1:
+            message_bit = "1 set_product entry was"
+        else:
+            message_bit = "%s set_product entries were" % queryset.count()
+        self.message_user(request, "%s successfully deleted." % message_bit)
+
+    really_delete_selected.short_description = "Delete selected entries (related image files will be deleted)"
+
+
+class ProductImageAdmin(admin.ModelAdmin):
+
+    actions = ['really_delete_selected']
+
+    def get_actions(self, request):
+        actions = super(ProductImageAdmin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
+
+    def really_delete_selected(self, request, queryset):
+        for obj in queryset:
+            obj.delete()
+
+        if queryset.count() == 1:
+            message_bit = "1 set_product_image entry was"
+        else:
+            message_bit = "%s set_product_image entries were" % queryset.count()
+        self.message_user(request, "%s successfully deleted." % message_bit)
+
+    really_delete_selected.short_description = "Delete selected entries (image file will be deleted)"
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Color)
 admin.site.register(Size)
 admin.site.register(Category)
-admin.site.register(ProductImage)
+admin.site.register(ProductImage, ProductImageAdmin)
 admin.site.register(ProductTags)
 # admin.site.register(ProductProperty)
