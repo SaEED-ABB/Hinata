560b6c23ec79bd47b7a7fee3a06ff0f2278dbe59
saeed <you@example.com>
2018-05-28 19:49:07 +0430

    fixed user panel page permissions

--- customer/views.py
+++ customer/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_http_methods
 
 from customer.models import User
-from .decorators import check_permission_render
+from .decorators import check_authentication_status
 
 
-@require_http_methods(['GET'])
-@check_permission_render(['user', 'admin'])
+@check_authentication_status()
 def user_panel(request, slug):
     user = get_object_or_404(User, uuid=slug)
     return render(request, 'customer/panel.html', {})
