3a79de91a8f3eaa8f952a59388f4bb0df59d3b89
mohammad <mohammad@arianatech.ir>
2018-05-31 13:51:13 +0430

    favorite page initialized

--- static/script/basket.js
+++ static/script/basket.js
@@ -3,6 +3,7 @@ $.ajax({
 	url: '/api/store/get_active_basket_info/',
 	dataType: 'JSON',
 	success: function (data) {
+		var total_count=0;
 		$('.basketContainer').html('');
 		$('.basketContainer').append('<div class="row">'+
 	    		'<div class="removeCol"></div>'+
@@ -14,27 +15,61 @@ $.ajax({
 	    	'</div>');
 			
 		for (var i =0; data.products.length >i; i++) {
+			total_count+=data.products[i].count;
 			$('.basketContainer').append('<div class="row" product_slug="'+data.products[i].slug+'">'+
 	    		'<div class="removeCol"><i class="fas fa-times" product_slug="'+data.products[i].slug+'"></i></div>'+
-	    		'<div class="prodCol"></div>'+
+	    		'<div class="prodCol">'+
+	    			'<h3>'+data.products[i].name+'</h3>'+
+	    			'<img src="'+data.products[i].image+'" class="prodImg">'+
+	    		'</div>'+
 	    		'<div class="colorCol">'+makeColors(data.products[i].colors, data.products[i].slug)+'</div>'+
 	    		'<div class="sizeCol">'+makeSizes(data.products[i].sizes, data.products[i].slug)+'</div>'+
-	    		'<div class="numCol">تعداد</div>'+
-	    		'<div class="priceCol">قیمت</div>'+
+	    		'<div class="numCol"><input  class="scale" type="number" value="'+data.products[i].count+'"></div>'+
+	    		'<div class="priceCol">'+data.products[i].price+'</div>'+
 	    	'</div>');
 		}
+		$('.basketContainer').append('<div class="row withoutBorder">'+
+	    		'<div class="removeCol"></div>'+
+	    		'<div class="prodCol"><button class="bigButton">خرید</button></div>'+
+	    		'<div class="colorCol"></div>'+
+	    		'<div class="sizeCol"></div>'+
+	    		'<div class="numCol">'+total_count+'</div>'+
+	    		'<div class="priceCol">'+data.total_price+'</div>'+
+	    	'</div>');
 	},
 	error: function(){
 
 	}
 });
 function makeColors(array,slug){
+	var text="";
 	for (var i =0; i < array.length; i++) {
-		$('.row[product_slug="'+slug+'"] .colorCol').append('<div class="color" color_slug="'+array[i].slug+'" style="background-color:'+array[i].code+'"></div>')
+		text+='<div class="color" color_slug="'+array[i].slug+'" style="background-color:'+array[i].code+'"></div>';
 	}
+	return text;
 }
 function makeSizes(array,slug){
+	var text="";
 	for (var i =0; i < array.length; i++) {
-		$('.row[product_slug="'+slug+'"] .sizeCol').append('<div class="size" size_slug="'+array[i].slug+'">'+array[i].name+'></div>')
+		text+='<div class="size" size_slug="'+array[i].slug+'">'+array[i].name+'</div>';
 	}
+	return text;
 }
+$(document).on('click','.fa-times',function(){
+	var parentElement=$(this).parent().parent()
+	$.ajax({
+		type: 'POST',
+		url: '/api/store/remove_from_basket/',
+		dataType: 'JSON',
+		data: {
+	        product_slug: $(this).attr('product_slug')
+	    },
+		success: function (data) {
+			parentElement.remove()
+			toastr.success('محصول مورد نظر از سبد خرید حذف شد.')
+		},
+		error: function(){
+			toastr.error('مشکلی خ داده است. لطفا ممجددا امتحان کنید.')
+		}
+	});
+})
---
+++ static/script/favorites.js
@@ -0,0 +1,11 @@
+$.ajax({
+	type: 'GET',
+	url: '/api/customer/get_favorites/',
+	dataType: 'JSON',
+	success: function (data) {
+		
+	},
+	error: function(){
+
+	}
+});
