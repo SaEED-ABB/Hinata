c18a2e13dd06779b898cfe729ff9e553da2f3012
mohammad <mohammad@arianatech.ir>
2018-06-03 18:42:06 +0430

    footer responsive completed

--- static/script/orders.js
+++ static/script/orders.js
@@ -14,15 +14,15 @@ $.ajax({
     		'</tr>');
 		for (var i =0; data.open.length >i; i++) {
 			$('.openOrdersTable').append('<tr class="oOrder'+i+'">'+
-    			'<td>'+i+'</td>'+
-    			'<td>KJ093640GS</td>'+
-    			'<td>27/06/1396 <br> 23:59</td>'+
+    			'<td>'+(i+1)+'</td>'+
+    			'<td>'+data.open[i].code+'</td>'+
+    			'<td> '+data.open[i].paid_at.day+'/'+data.open[i].paid_at.month+'/'+data.open[i].paid_at.year+' <br> '+data.open[i].paid_at.minute+':'+data.open[i].paid_at.second+'</td>'+
     			'<td>'+data.open[i].total_price+'</td>'+
-    			'<td>تهران-خیابان انقلاب-خیابان وصال-کوچه شاهد-پلاک 2</td>'+
+    			'<td>'+data.open[i].address+'</td>'+
     			statusTdMaker(data.open[i].status)+
     			'<td><i class="fas fa-angle-down"></i></td>'+
     		'</tr>'+
-    		'<tr class="oOrder'+i+'">'+
+    		'<tr class="oOrder'+i+'" style="display:none">'+
     			'<td colspan="7">'+
     				'<div class="secondTableContainer">'+
     					'<table>'+
@@ -44,31 +44,7 @@ $.ajax({
     							'<th>ارسال</th>'+
     							'<th>تحویل کالا</th>'+
     						'</tr>'+
-    						'<tr>'+
-    							'<td>'+
-    								'<img class="levelsImg" src="/static/img/img1.jpg">'+
-    							'</td>'+
-    							'<td>'+
-    								'<img class="levelsImg" src="/static/img/img2.jpg">'+
-    							'</td>'+
-    							'<td>'+
-    								'<img class="levelsImg" src="/static/img/img3.jpg">'+
-    							'</td>'+
-    							'<td>'+
-    								'<img class="levelsImg" src="/static/img/img4.jpg">'+
-    							'</td>'+
-    						'</tr>'+
-    						'<tr>'+
-    							'<td>'+
-    							'</td>'+
-    							'<td>'+
-    								'<div class="color" color_slug="'+data.open[i].products+'" style="background-color:red"></div>'+
-    							'</td>'+
-    							'<td>'+
-    								'<div class="size" size_slug="'+data.open[i].products+'">46</div>'+
-    							'</td>'+
-    							'<td>2</td>'+
-    						'</tr>'+
+    						makeStatusRows(data.open[i].status)+
     					'</table>'+
     				'</div>'+
     				'<div class="secondTableContainer"></div>'+
@@ -86,15 +62,15 @@ $.ajax({
     		'</tr>');
 		for (var i =0; data.closed.length >i; i++) {
 			$('.closedOrdersTable').append('<tr class="cOrder'+i+'">'+
-    			'<td>1</td>'+
-    			'<td>KJ093640GS</td>'+
-    			'<td>27/06/1396 <br> 23:59</td>'+
+    			'<td>'+(i+1)+'</td>'+
+    			'<td>'+data.closed[i].code+'</td>'+
+    			'<td> '+data.closed[i].paid_at.day+'/ '+data.closed[i].paid_at.month+'/ '+data.closed[i].paid_at.year+' <br> '+data.closed[i].paid_at.minute+':'+data.closed[i].paid_at.second+'</td>'+
     			'<td>'+data.closed[i].total_price+'</td>'+
-    			'<td>تهران-خیابان انقلاب-خیابان وصال-کوچه شاهد-پلاک 2</td>'+
+    			'<td>'+data.closed[i].address+'</td>'+
     			statusTdMaker(data.closed[i].status)+
     			'<td><i class="fas fa-angle-down"></i></td>'+
     		'</tr>'+
-    		'<tr class="cOrder'+i+'">'+
+    		'<tr class="cOrder'+i+'" style="display:none">'+
     			'<td colspan="7">'+
     				'<div class="secondTableContainer">'+
     					'<table>'+
@@ -116,31 +92,7 @@ $.ajax({
     							'<th>ارسال</th>'+
     							'<th>تحویل کالا</th>'+
     						'</tr>'+
-    						'<tr>'+
-    							'<td>'+
-    								'<img class="levelsImg" src="/static/img/img1.jpg">'+
-    							'</td>'+
-    							'<td>'+
-    								'<img class="levelsImg" src="/static/img/img2.jpg">'+
-    							'</td>'+
-    							'<td>'+
-    								'<img class="levelsImg" src="/static/img/img3.jpg">'+
-    							'</td>'+
-    							'<td>'+
-    								'<img class="levelsImg" src="/static/img/img4.jpg">'+
-    							'</td>'+
-    						'</tr>'+
-    						'<tr>'+
-    							'<td>'+
-    							'</td>'+
-    							'<td>'+
-    								'<div class="color" color_slug="'+data.closed[i].total_price+'" style="background-color:red"></div>'+
-    							'</td>'+
-    							'<td>'+
-    								'<div class="size" size_slug="'+data.closed[i].total_price+'">46</div>'+
-    							'</td>'+
-    							'<td>2</td>'+
-    						'</tr>'+
+    						makeStatusRows(data.closed[i].status)+
     					'</table>'+
     				'</div>'+
     				'<div class="secondTableContainer"></div>'+
@@ -198,4 +150,125 @@ function statusTdMaker(status){
 			return '<td class="greenBack">تحویل سفارش</td>'
 		}
 	}
+}
+function makeStatusRows(status){
+	var statusType=status.split('_')[0]
+	var statusLevel=status.split('_')[1]
+
+	if(statusLevel=='checking'){
+		return 	'<tr><td>'+
+    				'<img class="levelsImg" src="/static/img/img1.jpg"><br><img style="width:50px" src="/static/img/comp.gif">'+
+    			'</td><td>'+
+    				'<img class="levelsImg " src="/static/img/img2.jpg"><br><img class="tickImg" src="/static/img/more.png">'+
+    			'</td><td>'+
+    				'<img class="levelsImg" src="/static/img/img3.jpg"><br><img class="tickImg" src="/static/img/more.png">'+
+    			'</td><td>'+
+    				'<img class="levelsImg" src="/static/img/img4.jpg"><br><img class="tickImg" src="/static/img/more.png">'+
+    			'</td></tr>'+
+    			'<tr><td>'+
+    				'<span  class="linkButtonAllow">لغو سفارش</span>'+
+    			'</td><td>'+
+    				'<span  class="linkButton">لغو سفارش</span>'+
+    			'</td><td>'+
+    				
+    			'</td><td>'+
+    				'<span  class="linkButton">بازگشت کالا</span>'+
+    			'</td></tr>'
+	}else if(statusLevel=='preparing'){
+		return 	'<tr><td>'+
+    				'<img class="levelsImg" src="/static/img/img1.jpg"><br><img class="tickImg" src="/static/img/tick.png">'+
+    			'</td><td>'+
+    				'<img class="levelsImg" src="/static/img/img2.jpg"><br><img  style="width:50px" src="/static/img/comp.gif">'+
+    			'</td><td>'+
+    				'<img class="levelsImg" src="/static/img/img3.jpg"><br><img class="tickImg" src="/static/img/more.png">'+
+    			'</td><td>'+
+    				'<img class="levelsImg" src="/static/img/img4.jpg"><br><img class="tickImg" src="/static/img/more.png">'+
+    			'</td></tr>'+
+    			'<tr><td>'+
+    				'<span class="linkButtonDis">لغو سفارش</span>'+
+    			'</td><td>'+
+    				'<span  class="linkButtonAllow">لغو سفارش</span>'+
+    			'</td><td>'+
+    				
+    			'</td><td>'+
+    				'<span  class="linkButton">بازگشت کالا</span>'+
+    			'</td></tr>'
+	}else if(statusLevel=='sending'){
+		return 	'<tr><td>'+
+    				'<img class="levelsImg" src="/static/img/img1.jpg"><br><img class="tickImg" src="/static/img/tick.png">'+
+    			'</td><td>'+
+    				'<img class="levelsImg" src="/static/img/img2.jpg"><br><img class="tickImg" src="/static/img/tick.png">'+
+    			'</td><td>'+
+    				'<img class="levelsImg" src="/static/img/img3.jpg"><br><img  style="width:50px"  src="/static/img/comp.gif">'+
+    			'</td><td>'+
+    				'<img class="levelsImg" src="/static/img/img4.jpg"><br><img class="tickImg" src="/static/img/more.png">'+
+    			'</td></tr>'+
+    			'<tr><td>'+
+    				'<span  class="linkButtonDis">لغو سفارش</span>'+
+    			'</td><td>'+
+    				'<span  class="linkButtonDis">لغو سفارش</span>'+
+    			'</td><td>'+
+    				
+    			'</td><td>'+
+    				'<span  class="linkButton">بازگشت کالا</span>'+
+    			'</td></tr>'
+	}else if(statusLevel=='canceled'){
+		return 	'<tr><td>'+
+    				'<img class="levelsImg" src="/static/img/img1.jpg"><br><img class="tickImg" src="/static/img/tick.png">'+
+    			'</td><td>'+
+    				'<img class="levelsImg" src="/static/img/img2.jpg"><br><i class="fas fa-times-circle tickImg" style="font-size: 41px;color:red"></i>'+
+    			'</td><td>'+
+    				'<img class="levelsImg" src="/static/img/img3.jpg"><br><img class="tickImg" src="/static/img/more.png">'+
+    			'</td><td>'+
+    				'<img class="levelsImg" src="/static/img/img4.jpg"><br><img class="tickImg" src="/static/img/more.png">'+
+    			'</td></tr>'+
+    			'<tr><td>'+
+    				'<span  class="linkButtonDis">لغو سفارش</span>'+
+    			'</td><td>'+
+    				'<span  class="linkButtonDis">لغو سفارش</span>'+
+    			'</td><td>'+
+    				
+    			'</td><td>'+
+    				'<span  class="linkButtonDis">بازگشت کالا</span>'+
+    			'</td></tr>'
+	}else if(statusLevel=='returned'){
+		return 	'<tr><td>'+
+    				'<img class="levelsImg" src="/static/img/img1.jpg"><br><img class="tickImg" src="/static/img/tick.png">'+
+    			'</td><td>'+
+    				'<img class="levelsImg" src="/static/img/img2.jpg"><br><img class="tickImg" src="/static/img/tick.png">'+
+    			'</td><td>'+
+    				'<img class="levelsImg" src="/static/img/img3.jpg"><br><img class="tickImg" src="/static/img/tick.png">'+
+    			'</td><td>'+
+    				'<img class="levelsImg" src="/static/img/img4.jpg"><br><img class="tickImg" src="/static/img/tick.png">'+
+    			'</td></tr>'+
+    			'<tr><td>'+
+    				'<span  class="linkButtonDis">لغو سفارش</span>'+
+    			'</td><td>'+
+    				'<span  class="linkButtonDis">لغو سفارش</span>'+
+    			'</td><td>'+
+    				
+    			'</td><td>'+
+    				'<span  class="linkButtonDis">بازگشت کالا</span>'+
+    			'</td></tr>'
+	}else if(statusLevel=='delivered'){
+		return 	'<tr><td>'+
+    				'<img class="levelsImg" src="/static/img/img1.jpg"><br><img class="tickImg" src="/static/img/tick.png">'+
+    			'</td><td>'+
+    				'<img class="levelsImg" src="/static/img/img2.jpg"><br><img class="tickImg" src="/static/img/tick.png">'+
+    			'</td><td>'+
+    				'<img class="levelsImg" src="/static/img/img3.jpg"><br><img class="tickImg" src="/static/img/tick.png">'+
+    			'</td><td>'+
+    				'<img class="levelsImg" src="/static/img/img4.jpg"><br><img class="tickImg" src="/static/img/tick.png">'+
+    			'</td></tr>'+
+    			'<tr><td>'+
+    				'<span  class="linkButtonDis">لغو سفارش</span>'+
+    			'</td><td>'+
+    				'<span  class="linkButtonDis">لغو سفارش</span>'+
+    			'</td><td>'+
+    				
+    			'</td><td>'+
+    				'<span  class="linkButtonDis">بازگشت کالا</span>'+
+    			'</td></tr>'
+	}
+
 }
